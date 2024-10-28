2af068dc08c2b1144118fcd8acdaa1b66422799b
udezueoluomachi <basilchimaobi2@gmail.com>
2024-10-28 14:32:30 +0100

    How can I fix this ?

--- music.js
+++ music.js
@@ -1,9 +1,8 @@
 import YTMusic from "ytmusic-api";
-import ytdl from "@distube/ytdl-core";
 import fs from "fs"; // Required to manage file streams
 import path from "path";
 import { generate } from "randomstring";
-import scrapeYouTubeData from "auto-youtube-scraper-data";
+import { stream as Stream } from "play-dl";
 
 const API = new YTMusic();
 await API.initialize(/* Optional: Custom cookies */);
@@ -20,28 +19,6 @@ export default async function music(search, callback) {
     const videoId = songs[0].videoId
     
     const url = `https://www.youtube.com/watch?v=${songs[0].videoId}`;
-/*
-    scrapeYouTubeData(videoId, ({po, visitor}) => {
-      
-    })
-*/
-    const {PO_TOKEN, VISITOR_DATA} = {
-      PO_TOKEN : "MncnGdOz_nNUoiciIp-0WFtBT2y4I0mrob_G54piNeLkGXBEAaCltsEPAesTy-1_HDppCx4SWITGg7RdhySWrfvLGEBk4F72YJMobn-jafqXr1qt5go6GiuQZtusZGLmvgoo0BoaeHRLJViUEkhTCVqVmc55rYyrSg==",
-      VISITOR_DATA : "CgtzUWl0cFlRMlVPYyjqkPu4BjIKCgJORxIEGgAgVQ%3D%3D",
-    }
-    const headers = {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
-      "X-YouTube-Client-Name": "1",
-      "X-YouTube-Client-Version": "2.20201021.03.00",
-      "X-YouTube-Identity-Token": PO_TOKEN,
-      "X-YouTube-Page-CL": "00000000",
-      "X-YouTube-Page-Label": "youtube.ytfe.desktop_20201014_2_RC00",
-      "X-YouTube-Device": "cbr",
-      "X-YouTube-Utc-Offset": "0",
-      "X-YouTube-Visitor-Data": VISITOR_DATA,
-      Origin: "https://www.youtube.com",
-      Referer: "https://www.youtube.com/",
-    }
 
 
     const outputFilePath = `${songs[0].name}-${generate({length : 4})}.mp3`;
@@ -51,11 +28,7 @@ export default async function music(search, callback) {
       `${outputFilePath}`
     );
 
-    const stream = ytdl(url, {
-      quality: 'highestaudio',
-      filter: "audioonly",
-      headers
-    })
+    const {stream} = (await Stream(url))
 
     const audioOutput = fs.createWriteStream(audioPath);
 
